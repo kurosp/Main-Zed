977b37b9c150f21a692f5d5990ffecd37ce14d3d
мιин вαкα <thanh50687@gmail.com>
2022-01-03 18:24:21 +0700

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,12 @@
+  function hamDangNhap() {
+            window.open(
+              "./toandz/hehe.html", "_blank");
+		}
+  function FaceBook() {
+            window.open(
+              "https://facebook.com/toanhotface", "_blank");
+        }
+  function FaceBok() {
+            window.open(
+              "https://i.imgur.com/pSbUq7C.png", "_blank");
+        }
